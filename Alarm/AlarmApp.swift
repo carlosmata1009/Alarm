3800ccc4025ce06e3c6c65b05068707096f9f423
Carlos Mata <carlosmata1009@gmail.com>
2023-08-06 19:52:31 +0200

    Initial Commit

---
+++ Alarm/AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmApp.swift
+//  Alarm
+//
+//  Created by Carlos Mata on 8/6/23.
+//
+
+import SwiftUI
+
+@main
+struct AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
